2c5b62c7f1ae14a25a4d61f69035f2f77af9ec88
EDIMcGill <ishitahiremath@gmail.com>
2025-03-26 14:29:08 -0400

    update webpage description

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	site: 'https://EDIMcGill/', // Write here your website url
 	author: 'Ishita', // Site author
 	title: 'Equity Diversity and Includion for Graduate Students', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	description: 'Welcome to Team EDI McGill, Faculty of Engineering!', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
